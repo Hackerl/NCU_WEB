e894e516ae8fe9d6ab301c047fd9417ede8aa89b
Hackerl <hackerl@Hello-World.Hackerl.com>
2018-03-11 09:07:44 -0400

    start#coding page

---
+++ js/index.js
@@ -0,0 +1,27 @@
+(function($) {
+	'use strict';
+
+	$('#defend_btn').click(function () {
+		$(location).attr('href', 'login.html');
+    });
+    
+    $('#lost_found_btn').click(function () {
+		$(location).attr('href', 'login.html');
+    });
+
+    $('#wiki_btn').click(function () {
+		$(location).attr('href', 'login.html');
+    });
+
+    $('#suggest_btn').click(function () {
+		$(location).attr('href', 'login.html');
+    });
+
+    $('#contact_btn').click(function () {
+		$(location).attr('href', 'login.html');
+    });
+
+    $('#introduce_btn').click(function () {
+		$(location).attr('href', 'login.html');
+    });
+})(jQuery);
