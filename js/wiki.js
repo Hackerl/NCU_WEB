fcf685cedb1fe56fb0501c48d09f01ab9171b765
minajile <599085363@qq.com>
2018-03-12 00:45:37 +0800

    resolve toggle

--- js/wiki.js
+++ js/wiki.js
@@ -2,12 +2,16 @@
 	'use strict';
 
   $('.am-panel-hd').click(function () {
+		$(this).next().toggle(10);
+		$(this).children().children().toggle();
     $(".am-panel-hd").attr("class","am-panel-hd");
 		$(this).attr("class",' am-panel-hd am-panel-bd-hover');
-    $(".am-panel-bd-display").attr("class",'am-panel-bd')
-    $(this).next().attr("class",'am-panel-bd-display')
-    $('i.am-icon-angle-down').attr("class",'am-icon-angle-right am-icon-fw')
-    $(this).children().children().attr("class",'am-icon-angle-down am-icon-fw')
-    });
+    // $(".am-panel-bd-display").attr("class",'am-panel-bd')
+    // $(this).next().attr("class",'am-panel-bd-display')
+    // $('i.am-icon-angle-down').attr("class",'am-icon-angle-right am-icon-fw')
+    // $(this).children().children().attr("class",'am-icon-angle-down am-icon-fw')
+    }
+
+	);
 
   })(jQuery);
