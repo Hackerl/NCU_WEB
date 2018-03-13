6aaf0f576b145e868425a6211e01b8a550fc8613
Hackerl <hackerl@Hello-World.Hackerl.com>
2018-03-12 21:29:29 -0400

    add#form check

--- js/app/login.js
+++ js/app/login.js
@@ -5,10 +5,18 @@
 		var username = $("#username").val()
 		var password = $("#password").val()
 
-        var call_back = function(result){
-            $(location).attr('href', 'index.html');
-        }
-		post_json("/login",{"username": username, "password": password}, call_back)
+		if(username && password){
+			var call_back = function(result){
+				if(result.error == 0 ){
+					$(location).attr('href', 'index.html');
+				}else{
+					var $modal = $('#result-alert');
+					$modal.modal();
+					$('#post_result_text').text(result.msg);
+				}
+			}
+			post_json("/login",{"username": username, "password": password}, call_back)
+		}
 	});
 
 	$('#register_btn').click(function () {
