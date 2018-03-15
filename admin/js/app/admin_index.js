401e7907f9aebe4603b412012346ce3cb7d64403
Hackerl <hackerl@Hello-World.Hackerl.com>
2018-03-14 20:36:14 -0400

    coding#admin

---
+++ admin/js/app/admin_index.js
@@ -0,0 +1,34 @@
+(function($) {
+	'use strict';
+
+    if($.cookie('token') == null){ //如果当前有cookie
+      var token = localStorage.getItem("token") //取得cookie
+      if(token){
+        $.cookie('token', token, { expires: 10 }) //设置cookie
+      }
+    }
+
+	$('#defend_btn').click(function () {
+		$(location).attr('href', 'defend.html');
+    });
+    
+    $('#lost_found_btn').click(function () {
+		$(location).attr('href', 'lost_found.html');
+    });
+
+    $('#wiki_btn').click(function () {
+		$(location).attr('href', 'wiki.html');
+    });
+
+    $('#suggest_btn').click(function () {
+		$(location).attr('href', 'user.html');
+    });
+
+    $('#contact_btn').click(function () {
+		$(location).attr('href', 'chat.html');
+    });
+
+    $('#introduce_btn').click(function () {
+		$(location).attr('href', 'about.html');
+    });
+})(jQuery);
---
+++ admin/js/app/issues.js
@@ -0,0 +1,92 @@
+(function ($) {
+    'use strict';
+
+    var admin_get_all_issues = function () {  //获取全部issue
+        var call_back = function (result) {
+            if (result.error == 0) {
+                $.each(result.issues, function (index, obj) {
+                    switch (obj.status) {
+                        case 0:
+                            var issue = $('<tr><td>'+ obj.id +'</td><td>'+ obj.title +'</td><td><button type="button" class="am-btn am-btn-success">查看</button><button type="button" class="am-btn am-btn-primary">处理</button><button type="button" class="am-btn am-btn-warning">删除</button></td></tr>')
+                            $('#issues_table_waiting').append(issue);
+                            break;
+                        case 1:
+                            var issue = $('<tr><td>'+ obj.id +'</td><td>'+ obj.title +'</td><td><button type="button" class="am-btn am-btn-success">查看</button><button type="button" class="am-btn am-btn-primary">处理</button><button type="button" class="am-btn am-btn-warning">删除</button></td></tr>')
+                            $('#issues_table_working').append(issue);
+                            break;
+                        case 2:
+                            var issue = $('<tr><td>'+ obj.id +'</td><td>'+ obj.title +'</td><td><button type="button" class="am-btn am-btn-success">查看</button><button type="button" class="am-btn am-btn-primary">处理</button><button type="button" class="am-btn am-btn-warning">删除</button></td></tr>')
+                            $('#issues_table_finish').append(issue);
+                            break;
+                        default:
+                            break;
+                    }
+                }) // 显示所有issue
+
+                //绑定issue按钮
+                $('#issues_list').find('.am-btn-warning').click(function () {
+                    $('#delete-issue-confirm').modal({
+                        relatedTarget: this,
+                        onConfirm: function (e) {
+                            var issueid = $(this.relatedTarget).parent().parent().children()[0].innerText;
+                            var call_back = function (result) {
+                                if (result.error == 0) {
+
+                                } else {
+
+                                }
+                            }
+                            post_json("/delissue", { "issueid": issueid }, callback); //删除issue
+                        },
+                        onCancel: function (e) {
+                        }
+                    });
+                });
+
+                //绑定处理按钮
+                $('#issues_list').find('.am-btn-primary').click(function () {
+                    var $btn = $(this)
+                    var issueid = $btn.parent().parent().children()[0].innerText
+                    var $modal = $('#update-issue-prompt');
+                    $modal.modal();
+
+                    var update_status = $('#update_status').val();
+                    var update_comment = $('#update_comment').val();
+
+                    var call_back = function (result) {
+                        if (result.error == 0) {
+
+                        } else {
+
+                        }
+                    }
+                    //更新issue
+                    post_json("/updateissue", { "status": update_status, "comment": update_comment, "issueid": issueid }, callback);
+                });
+
+                //绑定查看按钮
+                $('#issues_list').find('.am-btn-success').click(function () {
+                    var $btn = $(this)
+                    var issueid = $btn.parent().parent().children()[0].innerText
+                    var $modal = $('#show-issue-modal');
+                    $modal.modal();
+
+                    var call_back = function (result) {
+                        if (result.error == 0) {
+                            $('#issue_content').text(result.content);
+                        } else {
+
+                        }
+                    }
+                    //更新issue
+                    post_json("/getissue", { "issueid": issueid }, callback);
+                });
+            }
+
+        }
+
+        get_json("/allissues", call_back);
+    }
+    admin_get_all_issues();
+
+})(jQuery);
