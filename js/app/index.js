86a53f0840b0377fb589195b613c688d648ce5cc
Hackerl <liupan052997@gmail.com>
2018-03-15 17:10:47 -0400

    finish#chatroom

--- js/app/index.js
+++ js/app/index.js
@@ -1,42 +1,42 @@
-(function($) {
-	'use strict';
+(function ($) {
+    'use strict';
 
-    if($.cookie('token') == null){ //如果当前有cookie
-      var token = localStorage.getItem("token") //取得cookie
-      if(token){
-        $.cookie('token', token, { expires: 10 }) //设置cookie
-      }
+    if ($.cookie('token') == null) { //如果当前有cookie
+        var token = localStorage.getItem("token") //取得cookie
+        if (token) {
+            $.cookie('token', token, { expires: 10 }) //设置cookie
+        }
     }
-    
-	  $('#defend_btn').click(function () {
-		$(location).attr('href', 'defend.html');
+
+    $('#defend_btn').click(function () {
+        $(location).attr('href', 'defend.html');
     });
-    
+
     $('#lost_found_btn').click(function () {
-		$(location).attr('href', 'lost_found.html');
+        $(location).attr('href', 'lost_found.html');
     });
 
     $('#wiki_btn').click(function () {
-		$(location).attr('href', 'wiki.html');
+        $(location).attr('href', 'wiki.html');
     });
 
     $('#suggest_btn').click(function () {
-		$(location).attr('href', 'user.html');
+        $(location).attr('href', 'user.html');
     });
 
     $('#contact_btn').click(function () {
-		$(location).attr('href', 'chat.html');
+        $(location).attr('href', 'chat.html');
     });
 
     $('#introduce_btn').click(function () {
-		$(location).attr('href', 'about.html');
+        $(location).attr('href', 'about.html');
     });
 
-    var get_userinfo = function(){
-      var callback = function(result){
-        
-      }
-      get_json("/userinfo", callback)
+    var get_userinfo = function () {
+        var callback = function (result) {
+
+        }
+        get_json("/userinfo", callback)
     }
     get_userinfo();
 })(jQuery);
